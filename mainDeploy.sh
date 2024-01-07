9786cae61037210b4d64dfdf091e401d9bf47723
xidong <chengxudong.cxd@digital-engine.com>
2024-01-07 19:13:37 +0800

    deploy

--- mainDeploy.sh
+++ mainDeploy.sh
@@ -8,6 +8,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:powerXuDong/Interesting-stuff.git main
+git push -f git@github.com:powerXuDong/Interesting-stuff.git master:main
 
 cd -
