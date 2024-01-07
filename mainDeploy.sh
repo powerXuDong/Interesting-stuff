a3692eb83acf70df16beea29b683435ae77ac020
xidong <chengxudong.cxd@digital-engine.com>
2024-01-07 19:05:51 +0800

    fix: 自动化更新分支名修改

--- mainDeploy.sh
+++ mainDeploy.sh
@@ -8,6 +8,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:powerXuDong/Interesting-stuff.git master:main
+git push -f git@github.com:powerXuDong/Interesting-stuff.git main
 
 cd -
